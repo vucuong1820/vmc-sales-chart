786f117138c0bbac7f5214311c169d1b627bcc62
Henry <henrytech4365@gmail.com>
2022-02-07 17:16:48 +0700

    Change from puppeteer to cheerio

--- pages/api/crawl/index.js
+++ pages/api/crawl/index.js
@@ -1,78 +1,78 @@
 import dbConnect from "../../../utils/dbConnect";
-import puppeteer from "puppeteer";
 import Customers from "../../../models/Customers";
 import format from "date-fns/format";
 import { themeShop } from "../../../constants/themeShop";
 import { getDateChart } from "../../../helpers/utils";
+import * as cheerio from "cheerio";
+import axios from "axios";
 dbConnect();
 
 export default async function handler(req, res) {
   const { shop } = req.query;
+
   const crawlData = async () => {
-    const getFilterShop = () => {
-      return themeShop.filter((item) => item.name === shop);
-    };
+    try {
+      const getFilterShop = () => {
+        return themeShop.filter((item) => item.name === shop);
+      };
 
-    const filterShop = getFilterShop();
-    const { fixedSales, name, themeId, url } = filterShop[0];
-
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url);
-    const presentSales = await page.evaluate(() => {
-      let titleLinks = document.querySelector(".item-header__sales-count");
-      return titleLinks.innerText;
-    });
-    const review = await page.evaluate(() => {
-      let review = document.querySelector(".is-visually-hidden");
-      return review.innerText;
-    });
-
-    await browser.close();
-    const getPreviousData = async () => {
-      const today = new Date();
-      const yesterday = new Date(today);
-
-      yesterday.setDate(yesterday.getDate() - 1);
-      const data = await Customers.find({
-        created_at: format(yesterday, "MM/dd/yyyy"),
+      const filterShop = getFilterShop();
+      const { fixedSales, name, themeId, url } = filterShop[0];
+      let presentSales;
+      let review;
+      await axios.get(url).then((res) => {
+        const $ = cheerio.load(res.data);
+        presentSales = $(".item-header__sales-count").text();
+        review = $(".is-visually-hidden").text();
       });
-      return data;
-    };
 
-    const previousDate = await getPreviousData();
-    const filterData = previousDate.filter((item) => item.name === name);
-    if (previousDate.length === 0) {
-      await Customers.findOneAndUpdate(
-        {
+      const getPreviousData = async () => {
+        const today = new Date();
+        const yesterday = new Date(today);
+
+        yesterday.setDate(yesterday.getDate() - 1);
+        const data = await Customers.find({
           created_at: format(yesterday, "MM/dd/yyyy"),
-          themeId: themeId,
-          name: name,
-        },
-        {
-          quantity: Number(presentSales.replace(/\D/g, "")) - fixedSales,
-          sales: Number(presentSales.replace(/\D/g, "")) - fixedSales,
-          review: Number(review.replace(/[^0-9\.]+/g, "")),
-        },
-        { upsert: true }
-      );
-    } else {
-      await Customers.findOneAndUpdate(
-        {
-          created_at: format(new Date(), "MM/dd/yyyy"),
-          themeId: themeId,
-          name: name,
-        },
-        {
-          quantity: Number(presentSales.replace(/\D/g, "")) - fixedSales,
-          sales:
-            Number(presentSales.replace(/\D/g, "")) -
-            fixedSales -
-            filterData[0].quantity,
-          review: Number(review.replace(/[^0-9\.]+/g, "")),
-        },
-        { upsert: true }
-      );
+        });
+        return data;
+      };
+
+      const previousDate = await getPreviousData();
+      const filterData = previousDate.filter((item) => item.name === name);
+      if (previousDate.length === 0) {
+        await Customers.findOneAndUpdate(
+          {
+            created_at: format(yesterday, "MM/dd/yyyy"),
+            themeId: themeId,
+            name: name,
+          },
+          {
+            quantity: Number(presentSales.replace(/\D/g, "")) - fixedSales,
+            sales: Number(presentSales.replace(/\D/g, "")) - fixedSales,
+            review: Number(parseFloat(review.match(/[\d\.]+/))),
+          },
+          { upsert: true }
+        );
+      } else {
+        await Customers.findOneAndUpdate(
+          {
+            created_at: format(new Date(), "MM/dd/yyyy"),
+            themeId: themeId,
+            name: name,
+          },
+          {
+            quantity: Number(presentSales.replace(/\D/g, "")) - fixedSales,
+            sales:
+              Number(presentSales.replace(/\D/g, "")) -
+              fixedSales -
+              filterData[0].quantity,
+            review: Number(review.match(/[\d\.]+/)),
+          },
+          { upsert: true }
+        );
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
   await crawlData();
