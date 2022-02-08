81be1cc55b84d756b9e07b7a8e4e242aaf7ec095
Henry <henrytech4365@gmail.com>
2022-02-08 10:52:56 +0700

    Update crawl automatically

--- pages/api/chart/index.js
+++ pages/api/chart/index.js
@@ -1,11 +1,11 @@
 import dbConnect from "../../../utils/dbConnect";
 import Customers from "../../../models/Customers";
-import format from "date-fns/format";
 
 dbConnect();
 
 export default async function handler(req, res) {
   const { startingDay, endingDay } = req.query;
+  
   await Customers.find({
     created_at: {
       $gte: startingDay,
