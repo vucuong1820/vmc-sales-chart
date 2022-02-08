6bd3ad25018adf0a5129b5438fb29cf8005e3195
Henry <henrytech4365@gmail.com>
2022-02-08 10:01:48 +0700

    Update crawl automatically

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const automation = async () => {
 };
 let now = new Date();
 let timer =
-  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 45, 0, 0) -
+  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 5, 0, 0) -
   now;
 
-setInterval(() => setTimeout(() => automation(), timer), 30000);
+setInterval(() => setTimeout(() => automation(), timer), 20000);
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
