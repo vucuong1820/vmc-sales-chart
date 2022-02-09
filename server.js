6a3a29caae88f24cab7f5aacc5883cafab946ea8
Henry <henrytech4365@gmail.com>
2022-02-09 14:37:27 +0700

    Test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const schedule = require("node-schedule");
 const crawlData = require("./helpers/crawlData.js");
 require("dotenv").config();
 console.log("======>", process.env.TIME)
-const job = schedule.scheduleJob("0 37 2 * * 0-6", async () => {
+const job = schedule.scheduleJob("0 39 14 * * 0-6", async () => {
   await crawlData();
 });
 //Ex: process.env.TIME=0 22 9 * * 0-6 ===> 9h22'0s am, Mon-Sun
