b83ae4fee6ad16bfd07fb8dd09e12d691ed3a5d2
Henry <henrytech4365@gmail.com>
2022-02-11 16:41:20 +0700

    Update crawl automatically

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,5 +2,7 @@ import schedule from "node-schedule";
 import { crawlData } from "../../helpers/crawlData";
 
 export default async function handler(req, res) {
-  await crawlData();
+  res.setHeader('Allow', 'GET');
+  await crawlData()
+  res.status(405).end('Method Not Allowed');
 }
