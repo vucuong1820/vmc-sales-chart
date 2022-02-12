643899286d61a7d4693f027b8690dcdf035f80a2
Henry <henrytech4365@gmail.com>
2022-02-12 11:09:39 +0700

    Update crawl automatically

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,5 @@
-import schedule from "node-schedule";
 import { crawlData } from "../../helpers/crawlData";
 
 export default async function handler(req, res) {
-  const data = await crawlData();
-  res.json(data);
+  await crawlData()
 }
