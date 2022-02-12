acea77f93794be57b0e69df916aa5927a87e0c1b
Henry <henrytech4365@gmail.com>
2022-02-12 21:00:35 +0700

    Update crawl and sending automatically

--- pages/api/auto-sending/index.js
+++ pages/api/auto-sending/index.js
@@ -6,40 +6,41 @@ import Customers from "../../../models/Customers";
 export default async function handler(req, res) {
   // await crawlData();
 
-  const getData = async () => {
-    const result = [
-      ["Minimog", 0, 0],
-      ["Wokiee", 0, 0],
-      ["Kalles", 0, 0],
-      ["Shella", 0, 0],
-      ["Gecko", 0, 0],
-      ["Ella", 0, 0],
-    ];
-    for (let i = 0; i < themeShop.length; i++) {
-      let time = getDateChart("this_week");
-      const data = await Customers.find({
-        created_at: {
-          $gte: time.start,
-          $lte: time.end,
-        },
-      }).exec();
-      const filterData = data.filter((item) => item.name === themeShop[i].name);
+  // const getData = async () => {
+  //   const result = [
+  //     ["Minimog", 0, 0],
+  //     ["Wokiee", 0, 0],
+  //     ["Kalles", 0, 0],
+  //     ["Shella", 0, 0],
+  //     ["Gecko", 0, 0],
+  //     ["Ella", 0, 0],
+  //   ];
+  //   for (let i = 0; i < themeShop.length; i++) {
+  //     let time = getDateChart("this_week");
+  //     const data = await Customers.find({
+  //       created_at: {
+  //         $gte: time.start,
+  //         $lte: time.end,
+  //       },
+  //     }).exec();
+  //     const filterData = data.filter((item) => item.name === themeShop[i].name);
 
-      result.forEach((item1) => {
-        filterData.forEach((item2) => {
-          if (item1[0] === item2.name) {
-            item1[1] = item2.review;
-            item1[2] += item2.sales;
-          }
-        });
-      });
+  //     result.forEach((item1) => {
+  //       filterData.forEach((item2) => {
+  //         if (item1[0] === item2.name) {
+  //           item1[1] = item2.review;
+  //           item1[2] += item2.sales;
+  //         }
+  //       });
+  //     });
 
-      result.sort((a, b) => {
-        return b[2] - a[2];
-      });
-    }
-    return result;
-  };
-  const boardData = await getData();
-  sendAlert(buildAlert(boardData));
+  //     result.sort((a, b) => {
+  //       return b[2] - a[2];
+  //     });
+  //   }
+  //   return result;
+  // };
+  // const boardData = await getData();
+  // sendAlert(buildAlert(boardData));
+  res.json("ABC");
 }
