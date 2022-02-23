50dabddeb97a52949c3639e232000260e0e76bec
Henry <henrytech4365@gmail.com>
2022-02-23 15:47:53 +0700

    Test

--- components/Table.jsx
+++ components/Table.jsx
@@ -13,7 +13,16 @@ const Table = ({ state }) => {
     ["Gecko", 0, 0],
     ["Ella", 0, 0],
   ]);
+
   const [rowsOfSlack, setRowsOfSlack] = useState([
+    ["Minimog", 0, 0],
+    ["Wokiee", 0, 0],
+    ["Kalles", 0, 0],
+    ["Shella", 0, 0],
+    ["Gecko", 0, 0],
+    ["Ella", 0, 0],
+  ]);
+  const [rowsOfSlackTest, setRowsOfSlackTest] = useState([
     ["Minimog", 0, 0],
     ["Wokiee", 0, 0],
     ["Kalles", 0, 0],
@@ -25,36 +34,41 @@ const Table = ({ state }) => {
   const [title, setTitle] = useState("");
 
   useEffect(() => {
-    let data = [];
+    let filterData = [];
 
     for (let i = 0; i < themeShop.length; i++) {
-      data.push(state.filter((item) => themeShop[i].name === item.name));
+      filterData.push(state.filter((item) => themeShop[i].name === item.name));
     }
-    data.forEach((theme) => {
-      let sale = 0;
-      let rating = 0;
-      let name;
-      const result = [...rowsUpdate];
-      themeShop.forEach((item) => {
-        for (let i = 0; i < theme.length; i++) {
-          if (item.name === theme[i].name) {
-            sale += theme[i].sales;
-            rating = theme[i].review;
-            name = item.name;
-          }
+
+    const result = [...rowsUpdate];
+
+    const data = filterData.map((theme) => {
+      return theme.reduce((prev, cur) => {
+        if (!prev[0] && !prev[1] && !prev[2]) {
+          prev[0] = "";
+          prev[1] = 0;
+          prev[2] = 0;
         }
-      });
-      result.forEach((item) => {
-        if (item[0] === name) {
-          item[2] = sale;
-          item[1] = rating;
-        }
-      });
-      result.sort((a, b) => {
-        return b[2] - a[2];
-      });
-      setRowsUpdate(result);
+        prev[0] = cur.name;
+        prev[1] = cur.review;
+        prev[2] += cur.sales;
+        return prev;
+      }, []);
     });
+
+    result.map((result) => {
+      data.forEach((test) => {
+        if (result[0] === test[0]) {
+          result[1] = test[1];
+          result[2] = test[2];
+        }
+      });
+    });
+
+    result.sort((a, b) => {
+      return b[2] - a[2];
+    });
+    setRowsUpdate(result);
 
     //Setting up title of table
     const filterDate = [
@@ -83,22 +97,62 @@ const Table = ({ state }) => {
   useEffect(() => {
     const getData = async () => {
       try {
-        themeShop.forEach(async (item) => {
-          const { data } = await axios.get(`/api/crawl?shop=${item.name}`);
-          const result = [...rowsOfSlack];
-          result.forEach((item1) => {
-            data.forEach((item2) => {
-              if (item1[0] === item2.name) {
-                item1[1] = item2.review;
-                item1[2] += item2.sales;
-              }
-            });
-          });
-          result.sort((a, b) => {
-            return b[2] - a[2];
-          });
-          setRowsOfSlack(result);
+        // Test
+        const filterData = [];
+        await Promise.all(
+          themeShop.map(async (theme) => {
+            const { data } = await axios.get(`/api/crawl?shop=${theme.name}`);
+            filterData.push(data);
+          })
+        );
+
+        const testResult = [...rowsOfSlackTest];
+        const testData = filterData.map((theme) => {
+          return theme.reduce((prev, cur) => {
+            if (!prev[0] && !prev[1] && !prev[2]) {
+              prev[0] = "";
+              prev[1] = 0;
+              prev[2] = 0;
+            }
+            prev[0] = cur.name;
+            prev[1] = cur.review;
+            prev[2] += cur.sales;
+            return prev;
+          }, []);
         });
+
+        testResult.map((result) => {
+          testData.forEach((test) => {
+            if (result[0] === test[0]) {
+              result[1] = test[1];
+              result[2] = test[2];
+            }
+          });
+        });
+
+        testResult.sort((a, b) => {
+          return b[2] - a[2];
+        });
+        setRowsOfSlackTest(testResult);
+        console.log(rowsOfSlackTest);
+        // Test
+
+        // themeShop.forEach(async (item) => {
+        //   const { data } = await axios.get(`/api/crawl?shop=${item.name}`);
+        //   const result = [...rowsOfSlack];
+        //   result.forEach((item1) => {
+        //     data.forEach((item2) => {
+        //       if (item1[0] === item2.name) {
+        //         item1[1] = item2.review;
+        //         item1[2] += item2.sales;
+        //       }
+        //     });
+        //   });
+        //   result.sort((a, b) => {
+        //     return b[2] - a[2];
+        //   });
+        //   setRowsOfSlack(result);
+        // });
       } catch (error) {
         console.log(error);
       }
@@ -106,7 +160,7 @@ const Table = ({ state }) => {
     getData();
   }, []);
   const handleClick = () => {
-    sendAlert(buildAlert(rowsOfSlack));
+    sendAlert(buildAlert(rowsOfSlackTest));
   };
 
   return (
